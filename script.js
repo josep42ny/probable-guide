6a8c760b8f618a57936e0926f4bc935e9bcab3f9
josep42ny <82282886+josep42ny@users.noreply.github.com>
2025-10-17 00:01:16 +0200

    fix:

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const QUOTES_PATH = 'https://zenquotes.io/api/today';
+const QUOTES_PATH = 'quotes.json';
 const quoteData = fetch(QUOTES_PATH)
   .then(data => data.json())
   .then(displayQuote)
   .catch(error => console.error(`Error while fetching data from ${QUOTES_PATH}:\n${error}`));
 
-function displayQuote(quote) {
-  const text = `${quote.q}\n\n-${quote.a}`;
+function displayQuote(quotes) {
+  const UNIX_DAYS = Math.floor(Date.now() / 1000 / 60 / 60 / 24);
+  const DAILY_QUOTE = UNIX_DAYS % quotes.length;
+  const quote = quotes[DAILY_QUOTE];
+  const text = `"${quote.quote}"\n\n-${quote.author}`;
 
   const quoteElem = document.createElement('P');
   quoteElem.innerText = text;
