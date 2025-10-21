416ed7cd4c03cd78a840387e0c414b6fb743788d
josep42ny <82282886+josep42ny@users.noreply.github.com>
2025-10-21 22:02:51 +0000

    feat: fetch api

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+import { WEATHER_CODES } from "./weatherCodes.js";
+
+const WEATHER_API = 'https://api.open-meteo.com/v1/forecast?latitude=39.625&longitude=3.1875&hourly=temperature_2m,weather_code&timezone=Europe%2FBerlin&timeformat=unixtime';
+const weatherData = await fetch(WEATHER_API)
+  .then(data => data.json());
+console.log(weatherData);
+const elem = document.querySelector('#weather');
+const hourly = weatherData.hourly;
+for (let i = 0; i < hourly.time.length; i++) {
+  const date = new Date(hourly.time[i] * 1000);
+  if (date.getHours() >= 8 && date.getHours() < 14) {
+    const cell = createCell(hourly.temperature_2m[i], hourly.weather_code[i]);
+    elem.appendChild(cell);
+  }
+}
+
+function createCell(temperature, weatherCode) {
+  const icon = document.createElement('I');
+  icon.classList = `wi ${WEATHER_CODES[weatherCode]}`;
+  const temp = document.createElement('P');
+  temp.innerText = `${temperature}°C`;
+
+  const wrapper = document.createElement('DIV');
+  wrapper.appendChild(icon);
+  wrapper.appendChild(temp);
+  return wrapper;
+}
+
+
+// Quotes
 const QUOTES_PATH = 'quotes.json';
 const quoteData = fetch(QUOTES_PATH)
   .then(data => data.json())
